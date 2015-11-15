e477ad5040a1ffa18c4feb1a07269be94ad673b5
Andy Waite <github.aw@andywaite.com>
2015-11-15 12:25:16 +0000

    Avoid DB hit in view specs

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -3,18 +3,27 @@ require "rails_helper"
 RSpec.describe "authors/index" do
   context "when there are authors to list" do
     it "lists the authors" do
-      authors = [
-        create(:author, name: "Martin Fowler"),
-        create(:author, name: "Uncle Bob")
-      ]
+      martin_fowler = build_stubbed(:author, name: "Martin Fowler")
+      uncle_bob = build_stubbed(:author, name: "Uncle Bob")
 
-      create(:book, author: authors[1], cover: "http://amz.on/clean_code.jpg")
-      create(:book, author: authors[1], cover: "http://amz.on/clean_coders.jpg")
-
-      render template: "authors/index", locals: { authors: authors }
+      render template: "authors/index",
+             locals: { authors: [martin_fowler, uncle_bob] }
 
       expect(rendered).to have_css("h2", text: "Martin Fowler")
       expect(rendered).to have_css("h2", text: "Uncle Bob")
+    end
+
+    it "lists the popular books by each author" do
+      uncle_bob = build_stubbed(:author, name: "Uncle Bob")
+      clean_code = build_stubbed(:book,
+                                 cover: "http://amz.on/clean_code.jpg")
+      clean_coders = build_stubbed(:book,
+                                   cover: "http://amz.on/clean_coders.jpg")
+      allow(uncle_bob).to receive(:popular_books).
+        and_return([clean_code, clean_coders])
+
+      render template: "authors/index", locals: { authors: [uncle_bob] }
+
       expect(rendered).to have_css("img[src*='http://amz.on/clean_code.jpg']")
       expect(rendered).to have_css("img[src*='http://amz.on/clean_coders.jpg']")
     end
