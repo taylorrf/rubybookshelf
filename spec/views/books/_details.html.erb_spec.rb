916924b39724332b7eafe48b27cfc39c3f63555e
Andy Waite <github.aw@andywaite.com>
2015-11-15 14:42:48 +0000

    Format summary paragraphs
    This will treat line breaks as separate paragraphs for easier reading.

--- spec/views/books/_details.html.erb_spec.rb
+++ spec/views/books/_details.html.erb_spec.rb
@@ -3,12 +3,15 @@ require "rails_helper"
 RSpec.describe "books/_details" do
   context "when the book has a summary" do
     it "displays the summary" do
-      book = build_stubbed(:book, summary: "The first book in the series")
-
+      book = build_stubbed(
+        :book, summary: "The first book in the series\nProbably the best"
+      )
       render template: "books/_details", locals: { book: book }
 
       expect(rendered).to have_css("p.summary",
                                    text: "The first book in the series")
+      expect(rendered).to have_css("p.summary",
+                                   text: "Probably the best")
     end
   end
 
